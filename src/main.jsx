f63d49103dfa1e7b087c917c4fe16f33e85aa0dd
farjana <farjanaCse74@gmail.com>
2024-10-09 23:23:02 +0000

    banner add in home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css';
 import {RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Router.jsx';
 
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <div className='container'>
     <RouterProvider router={router} />
+    </div>
   </StrictMode>,
 )
