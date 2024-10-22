2f17112c83e9eef603a4984d0ab93940798fc00e
farjana <farjanaCse74@gmail.com>
2024-10-22 00:33:23 +0000

    added slider in catagpry section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Banner from "../Banner/Banner";
+import Catagory from "../Catagory/Catagory";
+
 
 
 const Home = () => {
     return (
         <div className="container">
             <Banner></Banner>
+            <Catagory></Catagory>
         </div>
     );
 };
