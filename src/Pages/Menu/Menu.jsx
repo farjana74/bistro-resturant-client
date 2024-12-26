8a2c881e7f2c26021875269a4e1f80db0e504a39
farjana <farjanaCse74@gmail.com>
2024-12-26 01:18:44 +0000

    create shared menu item

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,10 +1,25 @@
 
+import PopularMenu from '../Home/PopularMenu/PopularMenu';
 import Cover from '../Shared/Cover/Cover';
+import MenuItem from '../Shared/MenuItem/MenuItem';
 
 const Menu = () => {
     return (
         <div>
             <Cover></Cover>
+            <PopularMenu></PopularMenu>
+            <br />
+            <br />
+            <Cover></Cover>
+            <PopularMenu></PopularMenu>
+            <br />
+            <br />
+            <Cover></Cover>
+            <PopularMenu></PopularMenu>
+            <br />
+            <br />
+            <Cover></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
