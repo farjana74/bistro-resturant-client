5fbec16586e3490c7a95dfbf62e82d8a57799080
farjana <farjanaCse74@gmail.com>
2025-01-03 01:27:52 +0000

    use shared component in contact form

--- src/Pages/ContactUs/ContactForm/ContactForm.jsx
+++ src/Pages/ContactUs/ContactForm/ContactForm.jsx
@@ -1,55 +1,50 @@
-import { Button, Col, Form, Row } from "react-bootstrap";
+import { Button, Col, FloatingLabel, Form, Row } from "react-bootstrap";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
+
 const ContactForm = () => {
+  const formStyle = {
+    BiBorderRadius:"8px",
+    border:"1px solid #E8E8E8",
+    backgroundColor:"#fff",
+    FontSize:"16px",
+    fontWeight:"400",
+    color:"#A1A1A1"
+  
+  }
   return (
     <div className="py-5  container">
       <SectionTitle subTitle="Send Us a Message" mainTitle="CONTACT FORM"></SectionTitle>
       <Form className="p-5 mt-2" style={{backgroundColor:"#F3F3F3"}}>
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Label>Name</Form.Label>
+          <Form.Control style={{formStyle}} type="email" placeholder="Enter Your Name" />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Label>Email</Form.Label>
+          <Form.Control style={{formStyle}} type="password" placeholder="Enter Your Password" />
         </Form.Group>
       </Row>
 
       <Form.Group className="mb-3" controlId="formGridAddress1">
-        <Form.Label>Address</Form.Label>
-        <Form.Control placeholder="1234 Main St" />
+        <Form.Label>Phone</Form.Label>
+        <Form.Control style={{formStyle}} placeholder="Enter Your Phone Number" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Address 2</Form.Label>
-        <Form.Control placeholder="Apartment, studio, or floor" />
-      </Form.Group>
+      <FloatingLabel
+        controlId="floatingTextarea"
+        label="Comments"
+        className="mb-3"
+      >
+        <Form.Control style={{formStyle}} as="textarea" placeholder="Leave a comment here" />
+      </FloatingLabel>
 
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridCity">
-          <Form.Label>City</Form.Label>
-          <Form.Control />
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Label>State</Form.Label>
-          <Form.Select defaultValue="Choose...">
-            <option>Choose...</option>
-            <option>...</option>
-          </Form.Select>
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridZip">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control />
-        </Form.Group>
-      </Row>
+      
 
       <Form.Group className="mb-3" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+        <Form.Check type="checkbox" label="I amn't a Robet" />
       </Form.Group>
 
       <Button variant="primary" type="submit">
