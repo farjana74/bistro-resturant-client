b2f1c0b72a45143824cf9c786ddeaa98315e485e
farjana <farjanaCse74@gmail.com>
2025-03-23 10:32:37 +0000

    after login redirect to menuitem

--- src/components/FoodCart/FoodCart.jsx
+++ src/components/FoodCart/FoodCart.jsx
@@ -2,15 +2,43 @@ import { Button, Card } from "react-bootstrap";
 import "./FoodCart.css";
 import useAuth from "../../hooks/useAuth";
 import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+
 
 const FoodCart = ({ item }) => {
-  const { name, recipe, image, category, price } = item;
+  const { name, recipe, image, category, price,_id } = item;
 
   const {user} = useAuth()
+  const navigate = useNavigate()
+  const location = useLocation()
+  const axiosSecure = useAxiosSecure()
   const handleAddToCart = (food) => {
     if(user && user.email){
-      // send
+      // send cart item to the database
+      const cartItem = {
+        menuId:_id,
+        email:user.email,
+        name,
+        image,
+        price
+      }
+      axiosSecure.post('/carts',cartItem)
+      .then(res=>{
+        console.log(res.data)
+        if(res.data.insertedId){
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${name} added to your cart`,
+            showConfirmButton: false,
+            timer: 1500
+          });
+        }
+      })
     }
+    
     else{
       Swal.fire({
         title: "You are not logged In",
@@ -19,14 +47,10 @@ const FoodCart = ({ item }) => {
         showCancelButton: true,
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
+        confirmButtonText: "Yes, Login!"
       }).then((result) => {
         if (result.isConfirmed) {
-          Swal.fire({
-            title: "Deleted!",
-            text: "Your file has been deleted.",
-            icon: "success"
-          });
+          navigate('/login',{state:{from:location}} )
         }
       });
     }
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,23 @@ import {RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Router.jsx';
 import AuthProvider from './providers/AuthProvider.jsx';
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query'
 
-
+// Create a client
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
+    <QueryClientProvider client={queryClient}>
     <div className=''>
     <RouterProvider router={router} />
     </div>
+    </QueryClientProvider>
+   
     </AuthProvider>
   </StrictMode>,
 )
