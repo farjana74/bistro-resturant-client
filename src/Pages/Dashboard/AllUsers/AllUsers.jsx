87d695bd12d5c0714b75bd3216e1a47ae37a23b5
farjana <farjanaCse74@gmail.com>
2025-05-03 18:07:25 +0000

    make admin

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -3,20 +3,61 @@ import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { Table } from "react-bootstrap";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import {FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: users = [] } = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
       return res.data;
     },
   });
-
-  const handleDelete = ()=>{
-    
+  const handleMakeAdmin =(user)=>{
+    axiosSecure.patch (`/users/admin/${user._id}`)
+ .then(res=>{
+  if(res.data.modifiedCount > 0){
+    refetch();
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: `${user.name} is an Admin Now!`,
+      showConfirmButton: false,
+      timer: 1500
+    });
   }
+ })
+  }
+  const handleDeleteUser = (user)=>{
+     Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            axiosSecure.delete(`/users/${user._id}`).then((res) => {
+              if (res.data.deletedCount > 0) {
+                refetch();
+                Swal.fire({
+                  title: "Deleted!",
+                  text: "Your file has been deleted.",
+                  icon: "success",
+                });
+              }
+            });
+          }
+        });
+
+  }
+  
+
+  
   return (
     <div>
       <div>
@@ -41,15 +82,25 @@ const AllUsers = () => {
             </tr>
           </thead>
           <tbody>
-            {users.map((item, index) => (
-              <tr key={item._id}>
+            {users.map((user, index) => (
+              <tr key={user._id}>
                 <th>{index + 1}</th>
-                <td>{item.name}</td>
-                <td>{item.email}</td>
-                <td>{item.role}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                {user.role === 'admin'? 'Admin' : <button
+                     onClick={() => handleMakeAdmin(user)}
+                    style={{ backgroundColor: "orange", border: 0 }}
+                  >
+                    <FaUsers
+                      style={{ height: "25px", width: "24px", color: "#fff" }}
+                    />
+                  </button>}
+
+                </td>
                 <th>
                   <button
-                    onClick={() => handleDelete(item._id)}
+                    onClick={() => handleDeleteUser(user)}
                     style={{ backgroundColor: "red", border: 0 }}
                   >
                     <RiDeleteBin6Line
