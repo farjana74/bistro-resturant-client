8549610e26ae1d6499e7666867b0c2ba2dd3ae70
farjana <farjanaCse74@gmail.com>
2025-05-17 21:30:01 +0000

    jwt integration

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -3,7 +3,7 @@ import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { Table } from "react-bootstrap";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
-import {FaUsers } from "react-icons/fa";
+import { FaUsers } from "react-icons/fa";
 import Swal from "sweetalert2";
 
 const AllUsers = () => {
@@ -11,71 +11,66 @@ const AllUsers = () => {
   const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
-      const res = await axiosSecure.get("/users",{
-        headers:{
-          authorization: `Bearer ${localStorage.getItem('access-token')}`
-        }
+      const res = await axiosSecure.get("/users", {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("access-token")}`,
+        },
       });
-     
+
       return res.data;
     },
   });
-  const handleMakeAdmin =(user)=>{
-    axiosSecure.patch (`/users/admin/${user._id}`)
- .then(res=>{
-  if(res.data.modifiedCount > 0){
-    refetch();
-    Swal.fire({
-      position: "top-end",
-      icon: "success",
-      title: `${user.name} is an Admin Now!`,
-      showConfirmButton: false,
-      timer: 1500
+  const handleMakeAdmin = (user) => {
+    axiosSecure.patch(`/users/admin/${user._id}`)
+    .then((res) => {
+      if (res.data.modifiedCount > 0) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} is an Admin Now!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
     });
-  }
- })
-  }
-  const handleDeleteUser = (user)=>{
-     Swal.fire({
-          title: "Are you sure?",
-          text: "You won't be able to revert this!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!",
-        }).then((result) => {
-          if (result.isConfirmed) {
-            axiosSecure.delete(`/users/${user._id}`).then((res) => {
-              if (res.data.deletedCount > 0) {
-                refetch();
-                Swal.fire({
-                  title: "Deleted!",
-                  text: "Your file has been deleted.",
-                  icon: "success",
-                });
-              }
+  };
+  const handleDeleteUser = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/users/${user._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
             });
           }
         });
+      }
+    });
+  };
 
-  }
-  
-
-  
   return (
     <div>
       <div>
-        
         <SectionTitle
           subTitle="---How Many---"
           mainTitle="MANAGE ALL USER"
         ></SectionTitle>
       </div>
-     
 
       <div className="shadow p-3">
-      <h3 className="pb-3">TOTAL USERS: {users.length}</h3>
+        <h3 className="pb-3">TOTAL USERS: {users.length}</h3>
         <Table className="w-100" bordered responsive="sm">
           <thead>
             <tr>
@@ -93,15 +88,18 @@ const AllUsers = () => {
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>
-                {user.role === 'admin'? 'Admin' : <button
-                     onClick={() => handleMakeAdmin(user)}
-                    style={{ backgroundColor: "orange", border: 0 }}
-                  >
-                    <FaUsers
-                      style={{ height: "25px", width: "24px", color: "#fff" }}
-                    />
-                  </button>}
-
+                  {user.role === "admin" ? (
+                    "Admin"
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      style={{ backgroundColor: "orange", border: 0 }}
+                    >
+                      <FaUsers
+                        style={{ height: "25px", width: "24px", color: "#fff" }}
+                      />
+                    </button>
+                  )}
                 </td>
                 <th>
                   <button
